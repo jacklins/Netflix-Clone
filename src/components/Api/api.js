12bd26badd567e172b9f8ae83ac8830b1909ed25
jacklins <juliocesar_19@hotmail.com.br>
2022-06-17 18:15:58 -0300

    ajustando api

--- src/components/Api/api.js
+++ src/components/Api/api.js
@@ -33,31 +33,31 @@ export default {
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28&language=pr-BR&api=${APIKEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${APIKEY}`)
             },
 
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35&language=pr-BR&api=${APIKEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${APIKEY}`)
             },
 
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27&language=pr-BR&api=${APIKEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${APIKEY}`)
             },
 
              {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749&language=pr-BR&api=${APIKEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${APIKEY}`)
             },
 
             {
                 slug: 'documentary',
                 title: 'Documentário',
-                items: await basicFetch(`/discover/movie?with_genres=99&language=pr-BR&api=${APIKEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${APIKEY}`)
             },
         ];
     },
